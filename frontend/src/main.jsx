009f44ca7d231eb83f4a4f5b5cccb41a687cebbf
Murilo Fischer <mufischer@hotmail.com>
2023-11-17 17:33:15 -0300

    Add react-redux to package.json and configure Redux store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// Redux
+
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </>,
 );
