20adcb8a6641be3eeb5a67fd6b58ee5479596e37
Murilo Fischer <mufischer@hotmail.com>
2023-11-17 09:06:49 -0300

    Add router and components for navigation

---
+++ frontend/src/pages/Auth/Register.jsx
@@ -0,0 +1,32 @@
+import "./Auth.css";
+
+// components
+import { Link } from "react-router-dom";
+
+// hooks
+import { useState, useEffect } from "react";
+
+const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos de seus amigos!</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem uma conta? <Link to="/login">Entre</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Register;
