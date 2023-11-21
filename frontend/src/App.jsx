128305d9c232a57d42d5b0a9e18271703ab2d1ad
Murilo Fischer <mufischer@hotmail.com>
2023-11-21 10:49:19 -0300

    Add useAuth hook for authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import "./App.css";
 //router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+// hooks
+import { useAuth } from "./hooks/useAuth";
+
 //components
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -13,6 +16,13 @@ import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 
 function App() {
+  const { auth, loading } = useAuth();
+  console.log(loading);
+
+  if (loading) {
+    return <p>carregando</p>;
+  }
+
   return (
     <>
       <div>
@@ -20,9 +30,18 @@ function App() {
           <Navbar />
           <div className="container">
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
+              <Route
+                path="/"
+                element={auth ? <Home /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/login"
+                element={!auth ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!auth ? <Register /> : <Navigate to="/" />}
+              />
             </Routes>
           </div>
           <Footer />
